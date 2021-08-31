be8a9046a7c38f084b5f7a92a9c9f07f01699d99
gerardo javier <gerardojao@gmail.com>
2021-08-31 13:10:02 -0400

    new changes

--- script.js
+++ script.js
@@ -47,3 +47,46 @@ d.addEventListener("click",e=>{
    }
     e.preventDefault()
 })
+
+
+const ls= localStorage;
+
+
+const darkTheme = (darkBtn)=>{
+    const themeBtn = d.querySelector(darkBtn),
+        selectors = d.querySelectorAll("[data-dark]")
+        console.log(themeBtn)
+    let sun ="☀️", moon="🌙" ;
+
+    const lightMode = ()=>{
+        selectors.forEach(el=>el.classList.remove("dark-mode"))   
+      
+        themeBtn.textContent=moon;
+        ls.setItem("theme","light")
+    }
+    const darkMode = ()=>{
+        selectors.forEach(el=>el.classList.add("dark-mode"))
+      
+        themeBtn.textContent=sun;
+        ls.setItem("theme","dark")
+    }
+    d.addEventListener("click",e=>{
+        if(e.target.matches(darkBtn)){
+            if(themeBtn.textContent===moon){
+                darkMode()
+             }else {
+                lightMode()
+            }
+        }
+     })
+
+    d.addEventListener("DOMContentLoaded",()=>{ 
+        if (ls.getItem("theme")==null)ls.setItem("theme","light")
+        if(ls.getItem("theme")=="light")lightMode()
+        if(ls.getItem("theme")=="dark")darkMode()
+
+    })
+    
+}
+
+ darkTheme(".dark-theme-btn")
